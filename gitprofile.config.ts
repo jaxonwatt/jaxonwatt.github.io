ec2793a16ad7a3ad376ae37f6673d0cecaa518ce
Emerald <mail@jaxonwatt.com>
2024-11-22 09:37:09 +1000

    color

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -189,11 +189,11 @@ const CONFIG = {
 
     // Custom theme, applied to `procyon` theme
     customTheme: {
-      primary: '#fc055b',
+      primary: '#1C4976',
       secondary: '#219aaf',
       accent: '#e8d03a',
       neutral: '#2A2730',
-      'base-100': '#E3E3ED',
+      'base-100': '#202020',
       '--rounded-box': '3rem',
       '--rounded-btn': '3rem',
     },
--- src/constants/default-custom-theme.tsx
+++ src/constants/default-custom-theme.tsx
@@ -1,9 +1,9 @@
 export const DEFAULT_CUSTOM_THEME = {
-  primary: '#fc055b',
+  primary: '#1C4976',
   secondary: '#219aaf',
   accent: '#e8d03a',
   neutral: '#2A2730',
-  'base-100': '#E3E3ED',
+  'base-100': '#202020',
   '--rounded-box': '3rem',
   '--rounded-btn': '3rem',
 };
